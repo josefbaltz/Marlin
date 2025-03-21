21c718237d11fc0f1061d951ef30d0a2b0355438
Josef Baltz <josef.baltz@proton.me>
2025-03-21 18:15:33 -0400

    Set Micro Swiss NG Specs pt1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1299,7 +1299,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 93.02 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 400 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
