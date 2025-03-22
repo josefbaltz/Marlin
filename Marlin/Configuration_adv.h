d75aa230c139af03fd25505f676c37c9e1f1f2dc
Josef Baltz <josef.baltz@proton.me>
2025-03-22 13:20:19 -0400

    Remove usage of MESH_INSET & manually set bounds

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2423,10 +2423,10 @@
 
 #if ANY(MESH_BED_LEVELING, AUTO_BED_LEVELING_UBL)
   // Override the mesh area if the automatic (max) area is too large
-  #define MESH_MIN_X MESH_INSET
-  #define MESH_MIN_Y MESH_INSET
-  #define MESH_MAX_X X_BED_SIZE - (MESH_INSET)
-  #define MESH_MAX_Y Y_BED_SIZE - (MESH_INSET)
+  #define MESH_MIN_X 95  //52mm from edge
+  #define MESH_MIN_Y 71  //52mm from edge
+  #define MESH_MAX_X 366 //52mm from edge
+  #define MESH_MAX_Y 337 //52mm from edge
 #endif
 
 #if ALL(AUTO_BED_LEVELING_UBL, EEPROM_SETTINGS)
