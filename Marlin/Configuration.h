29e9bf149aa757ea98908a0c5bed9fe4f4cb1e3b
Josef Baltz <josef.baltz@proton.me>
2025-03-22 12:28:43 -0400

    Adjust minimum and maximum axis.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1895,11 +1895,11 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS  8
-#define Y_MIN_POS  -4
+#define Y_MIN_POS  -2
 #define Z_MIN_POS  0
 #define X_MAX_POS  X_BED_SIZE + X_MIN_POS
 #define Y_MAX_POS  Y_BED_SIZE + Y_MIN_POS
-#define Z_MAX_POS  425        + Z_MIN_POS
+#define Z_MAX_POS  410        + Z_MIN_POS
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
