1a922532577f1edd70686b98173be2499eb1d42f
Josef Baltz <josef.baltz@proton.me>
2025-03-22 00:34:06 -0400

    invert endstops & XY kinematics

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1818,8 +1818,8 @@
 // @section motion
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
-#define INVERT_Y_DIR false
+#define INVERT_X_DIR true
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
@@ -1862,8 +1862,8 @@
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
-#define X_HOME_DIR  -1
-#define Y_HOME_DIR  -1
+#define X_HOME_DIR  1
+#define Y_HOME_DIR  1
 #define Z_HOME_DIR -1
 //#define I_HOME_DIR -1
 //#define J_HOME_DIR -1
